a89eddcee1347d646a291e18120498590690f04e
Wendi Ramos <wendiramos12@gmail.com>
2021-05-07 09:06:50 -0300

    .

---
+++ sequencial-02.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace raio_circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, A, pi = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            A = pi * R * R;
+
+            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
