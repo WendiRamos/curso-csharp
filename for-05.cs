6e01771d224d5a49c1fd8a0b879ba52462cdc914
Wendi Ramos <wendiramos12@gmail.com>
2021-05-07 12:38:22 -0300

    .

---
+++ for-05.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace for05 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int fat = 1;
+            for (int i = 1; i <= n; i++) {
+                fat = fat * i;
+            }
+
+            Console.WriteLine(fat);
+        }
+    }
+}
