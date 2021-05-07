1a994e0e3376a7ff04a644d5dec07b980793a50b
Wendi Ramos <wendiramos12@gmail.com>
2021-05-07 09:13:08 -0300

    .

---
+++ condicional-02.cs
@@ -0,0 +1,18 @@
+  
+using System;
+
+namespace cond02 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+
+            if (N % 2 == 0) {
+                Console.WriteLine("PAR");
+            }
+            else {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
