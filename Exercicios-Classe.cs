3fbf623ae76f56c3f6268a7ec6b8917b7578dd23
Wendi Ramos <81653845+WendiRamos@users.noreply.github.com>
2021-05-10 08:48:44 -0300

    .

---
+++ Exercicios-Classe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Atividade_curso_classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Entre os dados da primeira pessoa:");
+            Console.WriteLine("Nome:");
+            p1.nome = Console.ReadLine();
+            Console.WriteLine("Idade:");
+            p1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre os dados da primeira pessoa:");
+            Console.WriteLine("Nome:");
+            p2.nome = Console.ReadLine();
+            Console.WriteLine("Idade:");
+            p2.idade = int.Parse(Console.ReadLine());
+
+            if( p1.idade > p2.idade)
+            {
+                Console.WriteLine("Pessoa mais velha:" + p1.nome);
+            } else
+            {
+                Console.WriteLine("Pessoa mais velha:" + p2.nome);
+            }
+
+
+
+        }
+    }
+}
