8be5a4e9edb07b5e0ce38a9079c38e792e5369cc
Wendi Ramos <81653845+WendiRamos@users.noreply.github.com>
2021-05-10 12:53:29 -0300

    .

---
+++ static-01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using static_01;
+namespace ex01estatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+    /*class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+        public static double DolarParaReal(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * Iof / 100.0;
+        }
+    }*/
+}
