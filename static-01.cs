f838323ea2b0182ffc9a4c4b32f55136ed1bfddf
Wendi Ramos <wendiramos12@gmail.com>
2021-05-11 14:09:10 -0300

    .

--- static-01.cs
+++ static-01.cs
@@ -16,13 +16,5 @@ namespace ex01estatico
             Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
-    /*class ConversorDeMoeda
-    {
-        public static double Iof = 6.0;
-        public static double DolarParaReal(double quantia, double cotacao)
-        {
-            double total = quantia * cotacao;
-            return total + total * Iof / 100.0;
-        }
-    }*/
+    
 }
