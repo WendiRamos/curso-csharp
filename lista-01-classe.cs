fc574a493ce86434a0ad2e2a64c46b3e76fcc00e
Wendi Ramos <wendiramos12@gmail.com>
2021-05-12 12:29:44 -0300

    Create lista-01-classe.cs

---
+++ lista-01-classe.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+
+namespace listas_exercicio
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+        public void AumentoSalario(double porcentagem)
+        {
+            Salario = Salario + Salario * porcentagem / 100.0;
+        }
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
