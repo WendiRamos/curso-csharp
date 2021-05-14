b9fb102f6d2661b8f81bc82640162c841f97f83c
Wendi Ramos <wendiramos12@gmail.com>
2021-05-14 10:48:25 -0300

    .

---
+++ Testes/Programa Postagem(StringBuilder)/Entities/Comment.cs
@@ -0,0 +1,15 @@
+namespace Postagem.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
